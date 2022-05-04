8dcdb48be0f66309fc9615d2ad1a61a0baa833cd
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-05-04 16:41:16 +0100

    Done ! Con GET

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
 $frase ="La traduzione di var_dump è 'Variabile imbecille' ";
+$lunghezzaFrase = strlen ($frase);
+$uri = ( isset($_GET['parola'] ) ) ? $_GET['parola'] : ' ';
 
-$lunghezzaFrase = strlen ($frase)
+$parametro = $_GET['parametro'];
 
 ?>
 
@@ -12,18 +14,19 @@ $lunghezzaFrase = strlen ($frase)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Intro PhP</title>
 </head>
 <body>
+
 <h1>La frase è:</h1>
 <div>
         <?php
             echo $frase;
         ?>
 </div>
+
 <br>
 <h1>La lunghezza della frase è:</h1>
-
 <div>
         <?php
             echo $lunghezzaFrase;
@@ -31,15 +34,26 @@ $lunghezzaFrase = strlen ($frase)
 </div>
 
 <br>
-<h1>La frase corretta senza 'imbecille' è:</h1>
-
+<h1>La frase corretta senza 'imbecille' e senza GET è:</h1>
 <div>
         <?php
             echo str_ireplace("imbecille", "###", $frase);
         ?>
 </div>
 
+<br>
+<h1>Inserisci parola da eliminare nella frase</h1>
+<form method="GET">
+    <input type="text" name="parametro">
+    <button type="submit">Invia</button>
+</form>
 
+<h1>La frase corretta senza <?php $parametro ?> e con GET è:</h1>
+<div>
+        <?php
+            echo str_ireplace($parametro, "###", $frase);
+        ?>
+</div>
 
 
     
